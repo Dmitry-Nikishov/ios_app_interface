4c98e738fa5590b28e7acfe033a64893f2f8cea4
Dmitry Nikishov <dmitry86nik@gmail.com>
2022-02-16 00:41:16 +0300

    mapkit init project

---
+++ MapKit/MapKit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MapKit
+//
+//  Created by Дмитрий Никишов on 16.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
