0331b44cce46f92b56c5ee03430603e6470c2542
Dmitry Nikishov <dmitry86nik@gmail.com>
2022-03-04 20:31:01 +0300

    init template-based project

---
+++ FaceIdTest/FaceIdTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FaceIdTest
+//
+//  Created by Дмитрий Никишов on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
